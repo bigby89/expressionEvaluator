5a1e14f42ea4c2369c52f1c2819ebb869e8fae75
bigby89 <alex.89.turner@gmail.com>
2025-07-05 19:51:04 +0100

    Add CMAKE and prototype functions
    Created base files for expression evaluation and set up basic CMAKE files

---
+++ expressionEvaluator.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File: expressionEvaluator.cpp
+ * Author: Alex Turner
+ * Description: This file contains the implementation of the expression evaluator.
+ * It provides functions to evaluate mathematical expressions represented as strings.
+ * The expressions can include integers, basic arithmetic operations (+, -, *, /), and parentheses.
+ */
+
+// Include necessary headers
+#include "expressionEvaluator.hpp"
+
+// This function evaluates the expression and returns the result in 'result'.
+// It returns true if the expression is valid and false otherwise.
+int evaluate(const char* expression, int& result)
+{
+    // First, lets check if the expression is valid
+	// If the expression is invalid, we will return an error code
+
+    // parse the expression
+
+	// If we reach here, the expression was successfully evaluated
+}
---
+++ expressionEvaluator.hpp
@@ -0,0 +1,15 @@
+/*
+* ExpressionEvaluator.hpp
+* Author: Alex Turner
+* Description: This file contains the declarations of the expression evaluator.
+* It provides functions to evaluate mathematical expressions represented as strings.
+* The expressions can include integers, basic arithmetic operations (+, -, *, /), and parentheses.
+*/
+
+#ifndef EXPRESSION_EVALUATOR_HPP
+#define EXPRESSION_EVALUATOR_HPP
+#endif
+
+// This function evaluates the expression and returns the result in 'result'.
+// It returns true if the expression is valid and false otherwise.
+int evaluate(const char* expression, int& result);
